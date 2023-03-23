49b9d0537c83387358ad7ae448e1c82fabb16681
Von Harrison <vonharrison26@gmail.com>
2023-03-23 15:36:54 -0300

    ajuste map

--- src/Pages/Map/index.js
+++ src/Pages/Map/index.js
@@ -1,6 +1,8 @@
 import { collection, doc, getDoc, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import ReactMapGL, { Layer, Source } from "react-map-gl";
+import ReactMapGL, { Layer, Source,   GeolocateControl,
+  FullscreenControl,
+  NavigationControl, } from "react-map-gl";
 import { useParams } from "react-router-dom";
 import { db, storage } from "../../config/firebase";
 import Rede from "../../geojson/rede_antiga.geojson"
@@ -12,6 +14,7 @@ export function MapKML() {
   const [viewport, setViewport] = useState({});
   const params = useParams();
   const [workData, setWorkData] = useState([]);
+  const [teste, setTeste] = useState();
 
 
 
@@ -56,43 +59,46 @@ export function MapKML() {
     return item.id === params.id;
   });
 
-  async function getGeoJSON() {
-    try {
+  useEffect(() =>{
+    async function getGeoJSON() {
+  
       // Referência ao arquivo no armazenamento do Firebase
       const geoJSONRef = storage.ref('GEOJSON/rede_antiga.geojson');
       
-      geoJSONRef.getDownloadURL()
-    .then(url => {
-    console.log(url); // Imprime a URL do arquivo GeoJSON
-      fetch(url, {headers: {"Access-Control-Allow-Origin": "*"}, method: "no-cors"})
-      .then(response => {
-        const data = response.data;
-        console.log(data); // Imprime o conteúdo do arquivo GeoJSON
-     })
-  .catch(error => {
-    console.error(error);
-  });
-  })
-  .catch(error => {
-    console.error(error);
-  });
-     
-    } catch (error) {
-      console.error(error);
-    }
+      geoJSONRef.getDownloadURL().then(url => {
+        fetch("https://firebasestorage.googleapis.com/v0/b/e-gs-encibra.appspot.com/o/GEOJSON%2Frede_antiga.geojson?alt=media&token=051cd0fd-4616-4d3b-936c-2b48d8b319f1")
+          .then(response => response.json())
+          .then(data => setGeojsonData(data))
+          .catch(error => console.log(error));
+      }).catch(error => {
+        console.log(error);
+      });
+    
   }
 
-  getGeoJSON();
+  getGeoJSON()
+  },[])
 
-  const layerStylePara = {
-    id: "maine0",
-    type: "fill",
-    source: "maine",
-    layout: {},
-    paint: {
-      "fill-opacity": 0.1,
+
+
+
+
+  const rede2 = {
+    id: "line-layer",
+    type: "line",
+    source: "line-source",
+    layout: {
+      'line-join': 'round',
+'line-cap': 'round'
     },
+    paint: {
+      //  "fill-color": " #F4F3F1"
+      "line-color": "#11b4da",
+      'line-width': 3
+    },
+    
   };
+  console.log(workData)
 
   return (
     <>
@@ -106,7 +112,15 @@ export function MapKML() {
         mapStyle="mapbox://styles/mapbox/streets-v11"
         {...viewport}
         cooperativeGestures={true}
-      ></ReactMapGL>
+      >
+        <FullscreenControl position="bottom-right" />
+        <GeolocateControl position="bottom-right" />
+        <NavigationControl position="bottom-right" />
+
+        <Source id="line-layer" type="geojson" lineMetrics data={geojsonData}>
+          <Layer {...rede2} />
+        </Source>
+      </ReactMapGL>
     </>
   );
 }
